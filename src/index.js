8f1ed296c7dc918c0ca137cf81dd252c414c3eb1
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-18 20:40:08 +0100

    base commit, visual look done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.scss";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import LOGO_IMAGE from "./images/mars.png";
+
+class Body extends React.Component {
+  render() {
+    return (
+      <div className="banner">
+        <div className="navbar">
+          <img src={LOGO_IMAGE} className="logo"></img>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Our Method</a>
+            </li>
+            <li>
+              <a href="#">Why Policlock?</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="content">
+          <h1>TIME IN MARS:</h1>
+          <p>13/2/2011, 4:32:2PM</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Body />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
