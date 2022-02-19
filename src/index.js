ae4e752ee9cda7324aa5d39ef68040d24cd171c5
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-19 08:15:17 +0100

    move variables to separate scss, add info container with sections

--- src/index.js
+++ src/index.js
@@ -8,42 +8,88 @@ import LOGO_IMAGE from "./images/mars.png";
 class Body extends React.Component {
   render() {
     return (
-      <div className="banner">
+      <div id="home" className="home-container container-template">
         <nav className="navbar">
-          <a href="#">
+          <a href="#home">
             <img src={LOGO_IMAGE} className="logo"></img>
           </a>
           <ul>
             <li>
-              <a href="#">Home</a>
+              <a href="#home">Home</a>
             </li>
             <li>
-              <a href="#">Our Method</a>
+              <a href="#our-method">Our Method</a>
             </li>
             <li>
-              <a href="#">Why Policlock?</a>
+              <a href="#why-policlock">Why Policlock?</a>
             </li>
             <li>
-              <a href="#">About</a>
+              <a href="#about">About</a>
             </li>
           </ul>
         </nav>
 
-        <div class="content">
+        <div className="content">
           <h1>TIME IN MARS:</h1>
           <p>13/2/2011, 4:32:2PM</p>
         </div>
-        <div id="our-method">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
+      </div>
+    );
+  }
+}
+
+class OurMethod extends React.Component {
+  render() {
+    return (
+      <div id="our-method">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+    );
+  }
+}
+class WhyPoliClock extends React.Component {
+  render() {
+    return (
+      <div id="why-policlock">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+    );
+  }
+}
+class About extends React.Component {
+  render() {
+    return (
+      <div id="about">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+    );
+  }
+}
+class Info extends React.Component {
+  render() {
+    return (
+      <div className="info-container container-template">
+        <OurMethod />
+        <WhyPoliClock />
+        <About />
       </div>
     );
   }
@@ -65,8 +111,9 @@ class ToggleButton extends React.Component {
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div>
         <Body />
+        <Info />
         <ToggleButton />
       </div>
     );
