517a4f2ae39679e6739ec571e67579435c98e4d5
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-22 17:20:25 +0100

    add comments clarifying imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+// Packages
 import React from "react";
 
+// Components
 import Body from "./components/Body.js";
 import Info from "./components/Info.js";
 import Footer from "./components/Footer.js";
 
+// Styles
 import "./styles/index.scss";
 
 class App extends React.Component {
