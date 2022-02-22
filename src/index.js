0596da0d7acc6aa5cee8068c0f809ffa9ac92162
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-22 17:17:22 +0100

    move Body to components folder

--- src/index.js
+++ src/index.js
@@ -1,73 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Body from "./components/Body.js";
 import Info from "./components/Info.js";
 import Footer from "./components/Footer.js";
 
 import "./styles/index.scss";
-
-import LOGO_IMAGE from "./images/mars.png";
-const EARTH_TO_SOL = 0.97324429770083200222; // (24*3600) / (24*3600 + 39*60 + 35.244)
-
-// "a Martian day — is 24 hours, 39 minutes and 35.244 seconds,[3] equivalent to 1.02749125 Earth days."
-// https://en.wikipedia.org/wiki/Sol_(day_on_Mars)
-function getMarsMills() {
-  return new Date().getTime() * EARTH_TO_SOL;
-}
-
-class Body extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { milliseconds: getMarsMills() };
-  }
-  tick() {
-    this.setState({
-      milliseconds: getMarsMills(),
-    });
-  }
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-  render() {
-    return (
-      <div id="home" className="home-container container-template">
-        <nav role="navigation" className="navbar">
-          <a href="#home">
-            <img
-              src={LOGO_IMAGE}
-              alt="policlock logo, mars with stars around it"
-              className="logo"
-            ></img>
-            <h1>PoliClock</h1>
-          </a>
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#our-method">Our Method</a>
-            </li>
-            <li>
-              <a href="#why-policlock">Why Policlock?</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-          </ul>
-        </nav>
-
-        <main role="main" className="content">
-          <h1>TIME IN MARS:</h1>
-          <p>{new Date(this.state.milliseconds).toLocaleString()}</p>
-        </main>
-      </div>
-    );
-  }
-}
 
 class App extends React.Component {
   render() {
