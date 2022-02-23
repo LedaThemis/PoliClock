b6bab96e62efeda5cf434b1afb203659c55a4249
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-23 10:27:54 +0100

    add InfoCard class, use it as a template for info cards

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,56 +1,77 @@
 import React from "react";
 import "../styles/info.scss";
 
+class InfoCard extends React.Component {
+  render() {
+    return (
+      <div id={this.props.cardID} className="info-card">
+        <h2>{this.props.cardTitle}</h2>
+        <p>{this.props.cardDesc}</p>
+      </div>
+    );
+  }
+}
+
 class OurMethod extends React.Component {
   render() {
     return (
-      <div id="our-method" className="info-card">
-        <h2>Our Method</h2>
-        <p>
-          We calculate the date and time in Mars by calculating the time since
-          UNIX epoch in milliseconds, and converting that to Mars time. <br />
-          This isn't the conventional way to get date on Mars, but it allows you
-          to see the date on Mars in earth terms. <br />
-          Here's an{" "}
-          <a
-            href="https://www.giss.nasa.gov/tools/mars24/help/algorithm.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            article by NASA
-          </a>{" "}
-          with a more conventional/practical approach.
-        </p>
-      </div>
+      <InfoCard
+        cardID="our-method"
+        cardTitle="Our Method"
+        cardDesc={
+          <>
+            We calculate the date and time in Mars by calculating the time since
+            UNIX epoch in milliseconds, and converting that to Mars time. <br />
+            This isn't the conventional way to get date on Mars, but it allows
+            you to see the date on Mars in earth terms. <br />
+            Here's an{" "}
+            <a
+              href="https://www.giss.nasa.gov/tools/mars24/help/algorithm.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              article by NASA
+            </a>{" "}
+            with a more conventional/practical approach.
+          </>
+        }
+      />
     );
   }
 }
 class WhyPoliClock extends React.Component {
   render() {
     return (
-      <div id="why-policlock" className="info-card">
-        <h2>Why PoliClock?</h2>
-        <p>
-          Well, the secret formula to a good name is greek! (yes, that's true)
-          and with that, you take Mars (God of War) in greek which is "Polemos"
-          Adding to that "Clock", and you get PoleClock. Now, The problem with
-          PoleClock is the pronounciation, so we changed that to PoliClock 🎉
-        </p>
-      </div>
+      <InfoCard
+        cardID="why-policlock"
+        cardTitle="Why PoliClock?"
+        cardDesc={
+          <>
+            Well, the secret formula to a good name is greek! (yes, that's true)
+            and with that, you take Mars (God of War) in greek which is
+            "Polemos" Adding to that "Clock", and you get PoleClock. Now, The
+            problem with PoleClock is the pronounciation, so we changed that to
+            PoliClock 🎉
+          </>
+        }
+      />
     );
   }
 }
 class About extends React.Component {
   render() {
     return (
-      <div id="about" className="info-card">
-        <h2>About</h2>
-        <p>
-          This Project was inspired by a need to congratulate people in their
-          birthdays late and not get scolded, and that's possible by telling
-          them that it was in "Mars Time"
-        </p>
-      </div>
+      <InfoCard
+        cardID="about"
+        cardTitle="About"
+        cardDesc={
+          <>
+            This Project was inspired by a need to congratulate people in their
+            birthdays late and not get scolded, and that's possible by telling
+            them that it was in "Mars Time"
+          </>
+        }
+      />
     );
   }
 }
